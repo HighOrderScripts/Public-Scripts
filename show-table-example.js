ed4f24353703fefc262e1f2a6be4bd9299f5fe04
HighOrderScripts <112786015+HighOrderScripts@users.noreply.github.com>
2023-10-10 16:48:07 -0400

    Update show-table-example.js

--- show-table-example.js
+++ show-table-example.js
@@ -1,1 +1,5 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: desktop;
+importModule('AutomaticUpdate')
 (function(n){"use strict";function t(){}return n})(module.exports);
